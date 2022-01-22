8214480a76c2542e11bdf4b3d5f0fad9d792aa76
Sakshi Swati <sakshiswati33@gmail.com>
2022-01-22 15:51:28 +0530

    adding starting project

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Kabhi tumhe-female",
+    artist: "palak mauchal",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Shayad",
+    artist: "Arijit singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Tum hi Aana",
+    artist: "Jubin Nautiyal",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Tum se hi",
+    artist: "Ankit tiwari",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Ranjha",
+    artist: "Jasleen royal",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Raataan Lambiyaan",
+    artist: "Jubin Nautiyal",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
